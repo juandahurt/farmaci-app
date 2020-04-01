2152d83410e890ff79c735b71cab167b551e9210
Juan David Hurtado <juandahurtadom@gmail.com>
2020-04-01 17:50:44 -0500

    Configuración de base de datos para live demo

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,3 @@
 export default {
-    apiUrl: 'http://localhost:3000/api/'
+    apiUrl: 'https://farmaciapp-api.herokuapp.com/'
 }
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,8 +1,8 @@
 const db = {
-    name: 'farmaciapp_db',
-    host: 'localhost',
-    username: 'root',
-    password: 'password'
+    name: 'ppukumu7gvxivm0q',
+    host: 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'uxyc92fvqz4i5o7e',
+    password: 'diu9zgppgy19c8oi'
 };
 
 module.exports = db;
